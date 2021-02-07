7edf0bf8a0c7137a001172b325833a2ff4ad9b6f
ryuichi.matsuyama <mwtpjk@yahoo.co.jp>
2021-02-07 13:13:34 +0900

    Modify Index/NewServlet

--- src/controllers/IndexServlet.java
+++ src/controllers/IndexServlet.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.util.List;
 
 import javax.persistence.EntityManager;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -35,10 +36,17 @@ public class IndexServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		EntityManager em = DBUtil.createEntityManager();
-		List<Task> tasks = em.createNamedQuery("getALLTasks", Task.class).getResultList();
-		response.getWriter().append(Integer.valueOf(tasks.size()).toString());
-		em.close();
+	    EntityManager em = DBUtil.createEntityManager();
+
+	    List<Task> tasks = em.createNamedQuery("getAllTasks", Task.class).getResultList();
+
+	    em.close();
+
+	    request.setAttribute("tasks", tasks);
+
+	    RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/tasks/index.jsp");
+	    rd.forward(request, response);
+
 	}
 
 }
